e84808aae81304556b7bfa14d736ed6d6a4470f1
Martin Estevez <martin.estevez@udla.edu.ec>
2026-05-15 15:22:39 -0500

    Guardar todos los cambios pendientes

--- Practica1.c
+++ Practica1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(){
     int arreglo[]={3,6,9,12,15,16};
-    int *ptr=arreglo;
-
-    for(int i=0;i<6;i++){
-        printf("%d\n",arreglo[i]);
-            printf("%p\t", ptr );
-    }
+    int *ptr;
+    ptr =&arreglo[0];
+    int i= sizeof(int)  
+    printf("%d byte(s)\n", i);
+    int j= sizeof(arreglo);
+    printf("%d byte(s)%d",j);
     return 0;
 }
