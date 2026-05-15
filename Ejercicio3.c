92b52958dd0d8e4d657c61c7e4c0ae4ebdadaaab
Martin Estevez <martin.estevez@udla.edu.ec>
2026-05-15 15:20:21 -0500

    Ejercicio3 3 commit Ejercicio acabado

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -4,7 +4,7 @@
 int main(){
     char vocales []={'a','e','i','o','u'};
     char *ptr;
-    ptr = vocales;
+    ptr = vocales[3]; *ptr = 'x';
     for (int i=0;i < 6; i++) {
         printf("%c\n", vocales[i]);
     }
