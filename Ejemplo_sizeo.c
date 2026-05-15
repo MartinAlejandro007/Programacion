cfb9019a31b86025546957be7db36e04a1c81d79
Martin Estevez <martin.estevez@udla.edu.ec>
2026-05-15 15:07:10 -0500

    Ejemplo 2 comit 3 poniendo agregar ejercicio

--- Ejemplo_sizeo.c
+++ Ejemplo_sizeo.c
@@ -1,6 +1,21 @@
-#include "stdio.h"
+#include <stdio.h>
 int main(){
-    int i = sizeof(char);
-    printf("%d byte(s)", i);\
+    int arreglo[]={3,6,9,12,15,16};
+    int *ptr;
+    ptr =&arreglo[0];
+    int i= sizeof(int);
+    printf("%d byte(s)\n", i);
+    int j= sizeof(arreglo);
+    printf("%d byte(s)\n", j);
+    printf ("This computing environment uses: \n");
+
+printf("%zu byte for chars\n", sizeof(char)) ;
+printf("%zu bytes for shorts\n", sizeof(short int));
+printf("%zu bytes for ints\n", sizeof(int));
+printf("%zu bytes for longs\n", sizeof(long int));
+printf("%zu bytes for long longs\n", sizeof(long long int));
+printf ("%zu bytes for floats\n", sizeof (float));
+printf("%zu bytes for doubles\n", sizeof (double));
+printf("%zu bytes for pointers\n", sizeof(int *));
     return 0;
-}   
+}
