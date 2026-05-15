afa079825aff2348f3afeb7b1eb37307f0d36b84
Martin Estevez <martin.estevez@udla.edu.ec>
2026-05-15 15:15:10 -0500

    Ejercicio3 primer comit escribir el arrglo

---
+++ Ejercicio3.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+#include <math.h>
+
+int main(){
+    char vocales []={'a','e','i','o','u'};
+    for (int i=0;i < 6; i++) {
+        printf("%c\n", vocales[i]);
+    }
+    return 0;
+}
